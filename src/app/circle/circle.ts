d5c064ca7fdd73e279bc029914bf6576ec11787e
FerminDRA <72479156+FerminDRA@users.noreply.github.com>
2022-10-04 12:47:16 -0500

    Circle to Develop Closes #7 (#13)
    * create circle unit-test #7
    
    * update circle pass unit-tests #7

---
+++ src/app/circle/circle.ts
@@ -0,0 +1,11 @@
+export function circle(radio:number){
+    let pi=3.1416;
+    let res;
+    if(radio==0||radio<0){
+        res=0;
+    }
+    else {
+        res= pi*(radio**2);
+    }
+    return res;
+}
