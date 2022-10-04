230419f6bf679f7f6e987f2e30b7611ab13cbccf
FerminDRA <fermindelrosarioantonio@gmail.com>
2022-10-04 12:13:20 -0500

    create pow unit-test #6

---
+++ src/app/pow/pow.ts
@@ -0,0 +1,16 @@
+export function pow(x:number,n:number){
+    let res:number;
+    if(n==0){
+        res=5;
+    }
+    else if(n<0){
+        res=5;
+    }
+    else{
+        res=x
+        for(let i=1;i<n;i++){
+           res=5
+        }
+    }
+    return res;
+}
