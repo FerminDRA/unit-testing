4a390761b6c2e661448c6cbdc309c43307c28ec0
FerminDRA <fermindelrosarioantonio@gmail.com>
2022-10-04 12:14:55 -0500

    update pow pass unit-tests #6

--- src/app/pow/pow.ts
+++ src/app/pow/pow.ts
@@ -1,15 +1,15 @@
 export function pow(x:number,n:number){
     let res:number;
     if(n==0){
-        res=5;
+        res=1;
     }
     else if(n<0){
-        res=5;
+        res=0;
     }
     else{
         res=x
         for(let i=1;i<n;i++){
-           res=5
+           res=res*x
         }
     }
     return res;
